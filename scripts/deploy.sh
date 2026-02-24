53fe5c18a3b5e6f00474ff23afa1e39301f0ac73
roberdeth91 <amavizca.roberto@gmail.com>
2026-02-23 23:43:24 -0700

    change region to us-east-1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,7 +16,7 @@ cd terraform
 
 # New lines:
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-AWS_REGION=${DEFAULT_AWS_REGION:-us-east-2}
+AWS_REGION=${DEFAULT_AWS_REGION:-us-east-1}
 terraform init -input=false \
   -backend-config="bucket=twin-terraform-state-${AWS_ACCOUNT_ID}" \
   -backend-config="key=${ENVIRONMENT}/terraform.tfstate" \
